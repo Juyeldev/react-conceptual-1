518bd9973e7af2c9717715f1fe01ac30980ca0f3
Juyeldev <juyeldev53@gmail.com>
2023-04-12 12:00:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header'
+const App = () => {
+  return (
+    <div>
+     {/* header */}
+<Header></Header>
+     <Outlet/>
+     {/* footer */}
+    </div>
+  );
+};
+
+export default App;
